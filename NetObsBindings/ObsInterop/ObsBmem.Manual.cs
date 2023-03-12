802fc151037653a34573f57b1da7b545dc57afb8
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2023-03-12 14:05:47 +0200

    Add manual BMem functions

---
+++ NetObsBindings/ObsInterop/ObsBmem.Manual.cs
@@ -0,0 +1,53 @@
+﻿namespace ObsInterop;
+
+public static unsafe partial class ObsBmem
+{
+    public static T* bzalloc<T>()
+        where T: unmanaged
+    {
+        var size = sizeof(T);
+        return (T*) bzalloc((nuint) size);
+    }
+
+    public static void* bzalloc(nuint size)
+    {
+        var mem = bmalloc(size);
+        
+        if (mem != null)
+        {
+            var span = new Span<byte>(mem, (int)size);
+            span.Clear();
+        }
+
+        return mem;
+    }
+
+    public static char* bstrdup_n(char* str, nuint n)
+    {
+        if (str == null)
+            return null;
+
+        var dup = (char *)bmemdup(str, n + 1);
+        dup[n] = (char) 0;
+
+        return dup;
+    }
+
+    public static char *bstrdup(char *str)
+    {
+        if (str == null)
+            return null;
+
+        return bstrdup_n(str, strlen(str));
+    }
+
+    private static nuint strlen(char* str)
+    {
+        var initialPtr = str;
+        while (*(str++) != 0)
+        {
+        }
+
+        return (nuint) (str - initialPtr);
+    }
+}
