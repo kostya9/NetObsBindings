631a6e6d9fb648218112a179bfc6ae25afa227f0
Slkoshka <67478997+Slkoshka@users.noreply.github.com>
2023-04-09 00:21:41 +0400

    Add Linux support (#15)
    * Remove non-portable vararg bindings and add manual non-vararg alternative
    
    * Bump version number

---
+++ NetObsBindings/ObsInterop/ObsBase.Manual.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ObsInterop;
+
+public static unsafe partial class ObsBase
+{
+    // Due to a limitation in the .NET Runtime, there is currently no portable way to do P/Invoke with varargs in C#.
+    // Until https://github.com/dotnet/runtime/issues/48796 is resolved and support is added to ClangSharp, we can only
+    // provide non-varargs bindings to prevent silent portability bugs (i.e., crashes on Linux and possibly other
+    // platforms).
+    [DllImport("obs", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern void blog(int log_level, [NativeTypeName("const char *")] sbyte* format);
+}
--- NetObsBindings/ObsInterop/ObsBase.cs
+++ NetObsBindings/ObsInterop/ObsBase.cs
@@ -20,12 +20,6 @@ public static unsafe partial class ObsBase
     public static extern void base_set_crash_handler([NativeTypeName("void (*)(const char *, va_list, void *)")] delegate* unmanaged[Cdecl]<sbyte*, sbyte*, void*, void> handler, void* param1);
 
     [DllImport("obs", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    public static extern void blogva(int log_level, [NativeTypeName("const char *")] sbyte* format, [NativeTypeName("va_list")] sbyte* args);
-
-    [DllImport("obs", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    public static extern void blog(int log_level, [NativeTypeName("const char *")] sbyte* format, __arglist);
-
-    [DllImport("obs", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
     public static extern void bcrash([NativeTypeName("const char *")] sbyte* format, __arglist);
 
     [NativeTypeName("#define S__LINE__ STRINGIFY_(__LINE__)")]
